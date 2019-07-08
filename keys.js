9776fd4c7e62b595f4cc737f30a4979ce871a324
Zach Dunn <zfdunn@comcast.net>
2019-07-08 06:43:27 -0400

    spotify keys not working

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log("this is loaded");
 
 exports.spotify = {
-    id: process.env.c12730078b984c41a902624bcc98c3fd,
-    secret: process.env.361f0022c5c3404cb34e686d3962ad10
+    id: c12730078b984c41a902624bcc98c3fd,
+    secret: env361f0022c5c3404cb34e686d3962ad10
   };
--- liri.js
+++ liri.js
@@ -1,19 +1,15 @@
 // not sure what this does; saw this was required while watching hw guide on youtube
-require("dotenv").config();
+//      require("dotenv").config(); 
 // Add the code required to import the `keys.js` file and store it in a variable.
 var keys = require("./keys.js");
 // import npm packages: axios, moment, fs, spotify
 var axios = require("axios");
 var moment = require("moment");
 var fs = require("fs");
-var Spotify = require("node-spotify-api");
+var spotify = require("node-spotify-api");
 
 // initializing spotify keys
 var spotify = new spotify(keys.spotify);
-
-
-
-
 
 // liri's event listeners (1-4):
 // 1. 'concert-this':
@@ -22,112 +18,119 @@ var spotify = new spotify(keys.spotify);
 // Helper functions(a-d):
 //     a) will return user (name of artist),
 var uiArtist = function(artist) {
-    return artist.name;
+	return artist.name;
 };
 //     b) will return user with (song's name),
-var uiSong = function(songName){
-    if (songName === undefined){
-        return console.log("Error: " && err);
-    }
-    var song = data.tracks.items; 
-    var data = [];
-    for (var i = 0; i < song.length; i++){
-        data.push({
-            "arist(s): ": song[i].artists.map(uiArtist),
-            "song name: ": song[i].name,
-//     c) will return user (preview link of the song from spotify, album that the song is from);
-            "preview song: ": song[i].preview_url,
-//     d) will return user with(album that the song is from);
-            "album: ": song[i].album.name
-        });
-    }
-    console.log(data);
-    writeLog(data);
+var uiSong = function(songName) {
+	if (songName === undefined) {
+		return console.log("Error: " && err);
+	}
+	var song = data.tracks.items;
+	var data = [];
+	for (var i = 0; i < song.length; i++) {
+		data.push({
+			"arist(s): ": song[i].artists.map(uiArtist),
+			"song name: ": song[i].name,
+			//     c) will return user (preview link of the song from spotify, album that the song is from);
+			"preview song: ": song[i].preview_url,
+			//     d) will return user with(album that the song is from);
+			"album: ": song[i].album.name,
+		});
+	}
+	console.log(data);
+	writeLog(data);
 };
 uiSong();
 
 // 3. 'movie-this':
 
 // movie search function;
-var movie = function(movieName){
-    if (movieName === undefined){
-//     if no movie is provided, default should return "MR. NOBODY";
-        movieName = "Mr. Nobody";
-    };
+var movie = function(movieName) {
+	if (movieName === undefined) {
+		//     if no movie is provided, default should return "MR. NOBODY";
+		movieName = "Mr. Nobody";
+	}
 };
-// making axios requests to omdb 
-const urlApiKey = "=8b568eef"
-var url = ("http://omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey" + urlApiKey);
-axios.get(url).then(function(response) {
-    var jsonData = response.data;
-    // borrowing some of classmate's code
-//     will return user {
-    var data = {
-        //         title of movie, 
-        "Title: ": jsonData.Title,
-        //         year of movie, 
-        "Year: ": jsonData.Year,
-        //         rating of movie,
-        "Rated: ": jsonData.Rated,
-        //         imdb rating,
-        "IMDB Rating: ": jsonData.imdbRating,
-        //         country produced, 
-        "Country: ": jsonData.Country,
-        //         language of the movie, 
-        "Language: ": jsonData.Language, 
-        //         plot of the movie, 
-        "Plot: ": jsonData.Plot,
-        //         actors in the movie;
-        "Actors: ": jsonData.Actors,
-        //         rotten tomatoes rating of movie, 
-        "Rotten Tomatoes Raing: ": jsonData.Ratings[1].Value,
-    };
-    console.log(data);
-    writeLog;
-    console.log("");
-}).catch(function(error){
-    if (error) {
-        console.log("------Data-------");
-        console.log(error.response.data);
-        
-    }
-})
- 
+// making axios requests to omdb
+const urlApiKey = "=8b568eef";
+var url =
+	"http://omdbapi.com/?t=" +
+	movieName +
+	"&y=&plot=full&tomatoes=true&apikey" +
+	urlApiKey;
+axios
+	.get(url)
+	.then(function(response) {
+		var jsonData = response.data;
+		// borrowing some of classmate's code
+		//     will return user {
+		var data = {
+			//         title of movie,
+			"Title: ": jsonData.Title,
+			//         year of movie,
+			"Year: ": jsonData.Year,
+			//         rating of movie,
+			"Rated: ": jsonData.Rated,
+			//         imdb rating,
+			"IMDB Rating: ": jsonData.imdbRating,
+			//         country produced,
+			"Country: ": jsonData.Country,
+			//         language of the movie,
+			"Language: ": jsonData.Language,
+			//         plot of the movie,
+			"Plot: ": jsonData.Plot,
+			//         actors in the movie;
+			"Actors: ": jsonData.Actors,
+			//         rotten tomatoes rating of movie,
+			"Rotten Tomatoes Raing: ": jsonData.Ratings[1].Value,
+		};
+		console.log(data);
+		writeLog;
+		console.log("");
+	})
+	.catch(function(error) {
+		if (error) {
+			console.log("------Data-------");
+			console.log(error.response.data);
+		}
+	});
+
 // 4. 'do-what-it-says':
 //             will return user
 //             spotify-this-song: "I want it that way", as follows in the text in random.txt,
 //             edit the text in random.txt to test out the feature for movie-this, and concert-this
-    //         
-    var doWhatItSays = function(){
-        fs.readFile("random.txt", "utf8", function(err, data){
-            console.log(data);
-            var datA = data.split(",");
-            if (datA.length === 2){
-                pick (datA[0], datA[1]);
-                {else if (datA.length === 1){
-                    pick (datA[0]);    
-                };
-            };
-        };
-    });
-
-// Bonus:  
-//     output the data to a txt.file called "log.txt"
-var writeLog = function(data){
-
-//     append each command you run to the log.txt file
-//     do not overwrite your file each time you run a command
-fs.appendFile("log.txt", JSON.stringify(data) && "\n", function(err) {
-    if (err) {
-        return console.log(err);
-    }
-    console.log("UI successfully added to log.txt" && 
-    "\n=====================================\n");
-    });
+//
+var doWhatItSays = function() {
+	fs.readFile("random.txt", "utf8", function(err, data) {
+		console.log(data);
+		var datArr = data.split(",");
+		if (datArr.length === 2) {
+			pick(datArr[0], datArr[1]);
+		} else {
+			if (datArr.length === 1) {
+				pick(datArr[0]);
+			}
+		}
+	});
 };
 
+// Bonus:
+//     output the data to a txt.file called "log.txt"
+var writeLog = function(data) {
+	//     append each command you run to the log.txt file
+	//     do not overwrite your file each time you run a command
+	fs.appendFile("log.txt", JSON.stringify(data) && "\n", function(err) {
+		if (err) {
+			return console.log(err);
+		}
+		console.log(
+			"UI successfully added to log.txt" &&
+				"\n=====================================\n",
+		);
+	});
+};
 
-var runUI = function(argvOne, argvTwo){
-    pick(argvOne, argvTwo);
+var runUI = function(argvOne, argvTwo) {
+	pick(argvOne, argvTwo);
 };
 runUI(process.argv[2], process.argv.slice(3).join(" "));
